e87157456494ac06933c6a873381be23cfdf3ed0
kanishkagupta29 <122289785+kanishkagupta29@users.noreply.github.com>
2023-01-14 07:27:32 -0800

    submitting Q2 of assignment 1

---
+++ a1_q2.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int marks;
+    cout<<"Enter marks of student";
+    cin>>marks;
+    if(marks>90){
+        cout<<"Excellent";
+    }
+    else if(marks>80 && marks<=90){
+        cout<<"Good";
+    }
+    else if(marks>70 && marks<=80){
+        cout<<"Fair";
+    }
+    else if(marks>60 && marks<=70){
+        cout<<"Meets expectations";
+    }    
+    else{
+        cout<<"Below par";
+    }
+    return 0;
+}
